cc37a457e74606439bb71fdfcaeaa80b652a2890
A <aademolaoladipo@gmail.com>
2026-05-14 17:13:25 +0100

    harden checkout token: require secret and set 24h expiry

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -1,15 +1,27 @@
 package utils
 
 import (
+	"errors"
 	"os"
+	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
+const tokenTTL = 24 * time.Hour
+
 func GenerateToken(orderId string) (string, error) {
+	secret := os.Getenv("SECRET_KEY")
+	if secret == "" {
+		return "", errors.New("SECRET_KEY not set")
+	}
+
+	now := time.Now()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"orderId": orderId,
+		"iat":     now.Unix(),
+		"exp":     now.Add(tokenTTL).Unix(),
 	})
 
-	return token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+	return token.SignedString([]byte(secret))
 }
